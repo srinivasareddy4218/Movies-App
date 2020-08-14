b4c5a6725d7a64bdb317bbee6bd722c2d784534a
srinivasareddy4218 <63068380+srinivasareddy4218@users.noreply.github.com>
2020-08-14 16:40:28 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -7,5 +7,5 @@ const getProtocol = () => window.location.protocol;
 const getBackend = () => getProtocol() + "//" + BACKEND_NAME + getDomain();
 
 export const config = {
-  baseURL: getBackend()
+  baseURL: "gke-cluster-1-default-pool-7c4ce8f4-wvfs:31519"
 };
